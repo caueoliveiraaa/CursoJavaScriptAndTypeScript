035679ec78070940348f09fba2d248a9fd06e1c9
caueoliveiraaa <leo.siscobra@gmail.com>
2024-01-10 21:56:59 -0300

    Tipos primitivos

---
+++ aula1/02DataTypes.js
@@ -0,0 +1,23 @@
+// Tipos primitivos: String, Number, Undefined, Null, Boolean, Symbol
+const nome1 = 'Smith' // String
+const nome2 = "Smith" // String
+const nome3 = `Smith` // String
+const number1 = 55 // Number
+const number2 = 55.55 // Number
+// undefined = Não aponta para local algum na memória, como se fosse um valor nulo
+let nomeAluno 
+// nulo = Não aponta para local algum na memória, utilizamnos quando precisamos que a variável seja nula
+let sobrenomeAluno = null 
+let toggle1 = true // Boolean
+let toggle2 = false // Boolean
+// Verificando tipo
+console.log(typeof nome1, )
+console.log(typeof nome2, )
+console.log(typeof nome3, )
+console.log(typeof number1, )
+console.log(typeof number2, )
+console.log(typeof nomeAluno, nomeAluno)
+console.log(typeof sobrenomeAluno, sobrenomeAluno)
+console.log(typeof toggle1, toggle1)
+console.log(typeof toggle2, toggle2)
+
