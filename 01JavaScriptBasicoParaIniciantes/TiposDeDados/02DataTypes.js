a6d426263a6a065fe0b78eb854de46b4ac46e51d
caueoliveiraaa <leo.siscobra@gmail.com>
2024-04-29 20:45:25 -0300

    Update
    Update

--- 01JavaScriptBasicoParaIniciantes/TiposDeDados/02DataTypes.js
+++ 01JavaScriptBasicoParaIniciantes/TiposDeDados/02DataTypes.js
@@ -4,12 +4,15 @@ const nome2 = "Smith" // String
 const nome3 = `Smith` // String
 const number1 = 55 // Number
 const number2 = 55.55 // Number
+
 // undefined = Não aponta para local algum na memória, como se fosse um valor nulo
 let nomeAluno 
+
 // nulo = Não aponta para local algum na memória, utilizamnos quando precisamos que a variável seja nula
 let sobrenomeAluno = null 
 let toggle1 = true // Boolean
 let toggle2 = false // Boolean
+
 // Verificando tipo
 console.log(typeof nome1, )
 console.log(typeof nome2, )
